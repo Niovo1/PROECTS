d70cb843535ba8082702635b0972f20977df1040
Oibiri <ibirioniovosa@gmail.com>
2023-06-19 17:58:38 +0100

    first commit

---
+++ oniovosa_assignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class Student : IComparable<Student>
+{
+    public string Name { get; set; }
+    public int Marks { get; set; }
+
+    public int CompareTo(Student other)
+    {
+        // Compare students based on marks
+        return Marks.CompareTo(other.Marks);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student[] students = new Student[10]
+        {
+            new Student { Name = "Ken", Marks = 73 },
+            new Student { Name = "Matthew", Marks = 86 },
+            new Student { Name = "Charles", Marks = 61 },
+            new Student {Name = "James", Marks = 70},
+            new Student {Name = "Jerry", Marks = 80},
+            new Student {Name = "Jeremiah", Marks = 90},
+            new Student {Name = "Jacob", Marks = 76},
+            new Student {Name = "Jamie", Marks = 73},
+            new Student {Name = "Jane", Marks = 79},
+            new Student {Name = "John", Marks = 78}
+
+        };
+
+        Array.Sort(students);
+        
+
+        foreach (Student student in students)
+        {
+            Console.WriteLine($"Name: {student.Name}, Marks: {student.Marks}");
+        }
+    }
+}
---
+++ salary/Program.cs
@@ -0,0 +1,29 @@
+﻿using salary;
+using System;
+
+class Program : Worker
+{
+    static void Main() 
+    {
+        Worker[] workers = new Worker[10]
+        {
+            new Worker { Name = "Ken",      Salary = 4000 },
+            new Worker { Name = "Matthew",  Salary = 6900 },
+            new Worker { Name = "Charles",  Salary = 7000 },
+            new Worker {Name = "James",     Salary = 3900},
+            new Worker {Name = "Jerry",     Salary = 8000},
+            new Worker {Name = "Jeremiah",  Salary = 1500},
+            new Worker {Name = "Jacob",     Salary = 7800},
+            new Worker {Name = "Jamie",     Salary = 7100},
+            new Worker {Name = "Jane",      Salary = 7900},
+            new Worker {Name = "John",      Salary = 3800}
+        };
+
+        Array.Sort(workers, (w1, w2) => w2.Salary.CompareTo(w1.Salary));
+
+        foreach (Worker worker in workers)
+        {
+            Console.WriteLine($"Name: {worker.Name}, Salary: {worker.Salary}");
+        }
+    }
+}
